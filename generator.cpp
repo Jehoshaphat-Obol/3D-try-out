7e16f162befd6c882cfe749c3af1d367c8f5477d
Jehoshaphat Obol <jeholo5464@gmail.com>
2022-05-21 17:48:26 +0300

    the cube

---
+++ generator.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i = 0; i < 360; i++){
+        cout<<"            <img src="<<"images/heart.png " <<"style="\"--i: ""cls"<<i<<">\n";
+    }
+}
